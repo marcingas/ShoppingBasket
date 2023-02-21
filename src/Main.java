aea09b242118d5322b8026f59b8267e0da642afd
Marcin <gasiorek.mail@gmail.com>
2023-02-21 12:38:57 +0100

    new items added in Main class and change hasMap-> LinkedHashMap to store items in alphabetic order.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
+
+    private static StockList stockList = new StockList();
     public static void main(String[] args) {
+        StockItem temp = new StockItem("bread", 0.86, 100);
+        stockList.addStock(temp);
+        temp = new StockItem("cake", 1.10,70);
+        stockList.addStock(temp);
+        temp = new StockItem("carToy", 12.10,200);
+        stockList.addStock(temp);
+        temp = new StockItem("chair", 62.0,10);
+        stockList.addStock(temp);
+        temp = new StockItem("cup", 0.50,200);
+        stockList.addStock(temp);
+        temp = new StockItem("door", 72.95,40);
+        stockList.addStock(temp);
+        temp = new StockItem("juice", 2.50,40);
+        stockList.addStock(temp);
+        temp = new StockItem("phone", 94.0,30);
+        stockList.addStock(temp);
+        temp = new StockItem("towel", 1.43,50);
+        stockList.addStock(temp);
+        temp = new StockItem("vase", 9.99,70);
+        stockList.addStock(temp);
+        System.out.println(stockList);
+
+
 
 
     }
--- src/StockList.java
+++ src/StockList.java
@@ -1,11 +1,11 @@
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class StockList {
     private final Map<String,StockItem> list;
     public StockList(){
-        this.list = new HashMap<>();
+        this.list = new LinkedHashMap<>();
     }
     public int addStock(StockItem item){
         if(item != null){
@@ -43,9 +43,9 @@ public class StockList {
 
             double itemValue = stockItem.getPrice() * stockItem.quantityInStock();
             s = s + stockItem + ". There are " + stockItem.quantityInStock() + " in stock. Value of items: ";
-            s= s + itemValue + "\n";
+            s= s + String.format("%.2f",itemValue) + "\n";
             totalCost += itemValue;
         }
-        return s + "Total stock value" + totalCost;
+        return s + " Total stock value " + totalCost;
     }
 }
