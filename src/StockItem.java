a39489c20b9e6090c7f45a57114da0631c1c7c21
Marcin <gasiorek.mail@gmail.com>
2023-02-21 10:48:12 +0100

    new class StockList, new Map for storing StockItems, constructor for Map.

--- src/StockItem.java
+++ src/StockItem.java
@@ -3,12 +3,18 @@ import java.util.Comparator;
 public class StockItem implements Comparable<StockItem> {
     private final String name;
     private double price;
-    private int quantityStock = 0;
+    private int quantityStock;
 
     public StockItem(String name, double price) {
         this.name = name;
         this.price = price;
         this.quantityStock = 0;
+    }
+
+    public StockItem(String name, double price, int quantityStock) {
+        this.name = name;
+        this.price = price;
+        this.quantityStock = quantityStock;
     }
 
     public String getName() {
---
+++ src/StockList.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class StockList {
+    private final Map<String,StockItem> list;
+    public StockList(){
+        this.list = new HashMap<>();
+    }
+
+
+
+
+}
