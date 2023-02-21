7b3b05cd4cde1210bcf2d7d5b3f257a0d2fe9dee
Marcin <gasiorek.mail@gmail.com>
2023-02-21 11:00:12 +0100

    new method addStock

--- src/StockList.java
+++ src/StockList.java
@@ -6,6 +6,18 @@ public class StockList {
     public StockList(){
         this.list = new HashMap<>();
     }
+    public int addStock(StockItem item){
+        if(item != null){
+            StockItem inStock = list.getOrDefault(item.getName(),item);
+            //check if already have quantities of this item, adjust the quantity:
+            if(inStock != item){
+                item.adjustStock(inStock.getQuantityStock());
+            }
+            list.put(item.getName(), item);
+            return item.getQuantityStock();
+        }
+        return 0;
+    }
 
 
 
