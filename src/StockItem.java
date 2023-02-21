6f66a7b826a3d92ffe370e82c5c3868c6d85d055
Marcin <gasiorek.mail@gmail.com>
2023-02-21 10:02:15 +0100

    new class StockItem, fields, geters,setters, Constructor, method adjustStock,

---
+++ src/StockItem.java
@@ -0,0 +1,42 @@
+import java.util.Comparator;
+
+public class StockItem implements Comparable<StockItem> {
+    private final String name;
+    private double price;
+    private int quantityStock = 0;
+
+    public StockItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.quantityStock = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantityStock() {
+        return quantityStock;
+    }
+
+    public void setPrice(double price) {
+        if(price>0.0){
+            this.price = price;
+        }
+    }
+    public void adjustStock(int quantity){
+        int newQuantity = this.quantityStock + quantity;
+        if(newQuantity>=0){
+            this.quantityStock = newQuantity;
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        System.out.println();
+    }
+}
