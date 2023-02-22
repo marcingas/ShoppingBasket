44d2d7c14536129dac2691282404c3bf642623e6
Marcin <gasiorek.mail@gmail.com>
2023-02-22 09:59:22 +0100

    add reserve and unreserve method in StockList

--- src/StockList.java
+++ src/StockList.java
@@ -27,6 +27,23 @@ public class StockList {
         }
         return 0;
     }
+
+    public int reserveStock(String item, int quantity){
+        StockItem inStock = list.get(item);
+        if((inStock != null) && (quantity >0)){
+            return inStock.reserveStock(quantity);
+        }
+        return 0;
+
+    }
+
+    public int unreserveStock(String item, int quantity){
+        StockItem inStock = list.get(item);
+        if((inStock != null) && (quantity > 0)){
+            return inStock.unreserveStock(quantity);
+        }
+        return 0;
+    }
     public StockItem get(String key){
         return list.get(key);
     }
